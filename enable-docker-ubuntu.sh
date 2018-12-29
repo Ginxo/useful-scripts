e76838a3c746ab2e8c609c4e5439ca10f1b33fc0
Enrique Mingorance Cano <ginxaco@gmail.com>
2018-12-29 11:32:54 +0100

    enable-docker-ubuntu.sh file, comments added

--- enable-docker-ubuntu.sh
+++ enable-docker-ubuntu.sh
@@ -1,3 +1,6 @@
+# Enables docker service as a normal user
+# Sources:
+# https://github.com/docker/docker-snap
 sudo snap connect docker:home
 sudo addgroup --system docker
 sudo adduser $USER docker
